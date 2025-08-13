857bea89e052580fb5d3c80f3de0399beb78d536
ChoTamong <jogp0110@naver.com>
2025-08-13 10:08:37 +0900

    플레이어 이동 및 카메라

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float JumpPower;
+    private Vector2 curMovementInput;
+    public LayerMask groundLayerMask;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float minLook;
+    public float maxLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+    public Vector2 mousedelta;
+    public bool canLook = true;
+
+    public Action inventory;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+    private void LateUpdate()
+    {
+        CameraLook();
+    }
+    void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = _rigidbody.velocity.y;
+
+        _rigidbody.velocity = dir;
+    }
+
+    void CameraLook()
+    {
+        camCurXRot += mousedelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minLook, maxLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mousedelta.x * lookSensitivity, 0);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    public void Onlook(InputAction.CallbackContext context)
+    {
+        mousedelta = context.ReadValue<Vector2>();
+    }
+}
